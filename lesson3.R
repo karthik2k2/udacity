4337ef0f6ab4f7750def091e3baf2fcce893aaa2
Karthik <karthik2k2@outlook.com>
2015-10-17 01:14:08 +0530

    Adding lesson 3
    This is a lesson and committing to github

--- lesson3.R
+++ lesson3.R
@@ -16,4 +16,20 @@ ggplot(data=subset(pf,!is.na(gender)),aes(x=friend_count))+geom_histogram(binwid
 #using by command to apply to function by categories to one variable
 table(pf$gender)
 by(pf$friend_count,pf$gender,summary)
+#plotting tenure 
+ggplot(data=pf,aes(x=tenure/365))+
+  geom_histogram(colour='black',fill='#099DD9',binwidth=0.25)+
+  scale_x_continuous(limits=c(0,8),breaks=seq(0,8,1))+
+  xlab("Number of years using facebook")+
+  ylab("Number of users in sample")
+
+#histogram of ages
+ggplot(data=pf,aes(x=age))+
+  geom_histogram(binwidth=5,colour='black',fill='#099DD9')+
+  scale_x_continuous(limits=c(0,120),breaks=seq(0,120,10))+
+  theme_igray()+
+  scale_colour_tableau()+
+  xlab("Age of Facebook Users")+
+  ylab("Count of users in the sample")
+
 
--- twoVariables.R
+++ twoVariables.R
@@ -4,8 +4,14 @@ pf <- read.csv("pseudo_facebook.tsv", sep="\t")
 #plotting age and friend count
 qplot(x=age,y=friend_count,data=pf)
 
+#theme setting
+library(ggthemes)
+ggthemr("flat")
+
 #More formal ggplot syntax
-ggplot(aes(x=age,y=friend_count),data=pf)+geom_jitter(alpha=1/20) +
-        xlim(13,90)+
-        ylim(0,5000)+
-        coord_trans(y="sqrt")
+ggplot(diamonds, aes(x=price, fill=cut)) +
+        geom_histogram(position="dodge", binwidth=1000) +
+        scale_fill_brewer(palette = "Greens")+
+        ylab("Number of diamonds")+theme_wsj(base_size=12)+
+        theme(legend.text = element_text(size = 15),
+              legend.title=element_text(size=12))
