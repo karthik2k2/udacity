6a41c661dafc230dae9f906518ac3f944aeb012e
Karthik <karthik2k2@outlook.com>
2016-03-13 14:12:18 +0530

    Some changes made from Visual studio 2015

--- twoVariables.R
+++ twoVariables.R
@@ -5,13 +5,15 @@ pf <- read.csv("pseudo_facebook.tsv", sep="\t")
 qplot(x=age,y=friend_count,data=pf)
 
 #theme setting
-library(ggthemes)
+library(ggthemr)
 ggthemr("flat")
 
 #More formal ggplot syntax
 ggplot(diamonds, aes(x=price, fill=cut)) +
         geom_histogram(position="dodge", binwidth=1000) +
         scale_fill_brewer(palette = "Greens")+
-        ylab("Number of diamonds")+theme_wsj(base_size=12)+
+        ylab("Number of diamonds")+theme_economist(base_size=12)+
         theme(legend.text = element_text(size = 15),
-              legend.title=element_text(size=12))
+              legend.title = element_text(size = 12))
+              # Some new changes are added
+head(pf)
